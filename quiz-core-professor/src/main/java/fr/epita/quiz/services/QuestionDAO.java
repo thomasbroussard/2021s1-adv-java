9043a5403277985e519f01317083dec5b29b9027
thomas <thomas.broussard@local.local>
2021-04-06 12:11:57 +0200

    * dao structure

--- quiz-core-professor/src/main/java/fr/epita/quiz/services/QuestionDAO.java
+++ quiz-core-professor/src/main/java/fr/epita/quiz/services/QuestionDAO.java
@@ -1,26 +1,39 @@
 package fr.epita.quiz.services;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.sql.DataSource;
+
 
 import fr.epita.quiz.datamodel.Question;
 
 public class QuestionDAO {
 	
-
+	@Inject
 	DataSource datasource;
-
-	public void setDatasource(DataSource datasource) {
-		this.datasource = datasource;
-	}
 
 	public void createQuestion(Question question) throws SQLException {
 		Connection connection = this.datasource.getConnection();
+			
+		
+	}
+	public void deleteQuestion(Question question) throws SQLException {
+		Connection connection = this.datasource.getConnection();
+		
+		
+	}
+	public void updateQuestion(Question question) throws SQLException {
+		Connection connection = this.datasource.getConnection();
+		
+		
+	}
+	public List<Question> searchQuestion(Question question) throws SQLException {
+		Connection connection = this.datasource.getConnection();
+		
+		return null;
 		
 	}
 
